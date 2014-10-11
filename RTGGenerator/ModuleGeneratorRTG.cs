52e08dccaf4aeb1eb6097b2f458689e5ed03bc47
Gribbleshnibit8 <gribbleshnibit8@gmail.com>
2014-10-11 00:35:31 -0500

    Changed from selectable fuel to .cfg set options.

--- RTGGenerator/ModuleGeneratorRTG.cs
+++ RTGGenerator/ModuleGeneratorRTG.cs
@@ -28,147 +28,104 @@ namespace RTGGenerator
     [KSPModule("RTG Power Supply")]
     public class ModuleGeneratorRTG : PartModule
     {
-        
-        static private string[] FuelTypes = { "Blutonium", "Karborundum", "Kerbaricium" };
-        static private decimal[] EnergyDensity = { 0.46M, 0.54M, 0.135M };
-        static private double[] Halflives = { 8.352, 25.433, 125.28 };
-        static private string[] ThermocoupleTypes = { "Kerbanium Telluride", "Thermionic", "Thermophotovoltaic" };
-        static private double[] ThermocoupleEfficiencies = { 0.05, 0.18, 0.3 };
-        
-        // index of the fuel type selected, 
-        [KSPField(isPersistant = true)]
-        private int selectedFuel = 0;
-
-        // index of the thermocouple type selected, 
-        [KSPField(isPersistant = true)]
-        private int selectedThermocouple = 0;
-
-    // information output
-        [KSPField(isPersistant = false, guiActiveEditor = true, guiActive = true, guiName = "Power Output", guiUnits = " Ec/s")]
-        private double PowerOutput = 0;
-
-        [KSPField(isPersistant = false, guiActiveEditor = true, guiName = "Fuel")]
-        public string CurrentFuel = String.Empty;
-
-        [KSPField(isPersistant = false, guiActiveEditor = true, guiActive = true, guiName = "Half-life", guiUnits = " years")]
-        public double Halflife = 0;
-
-        [KSPField(isPersistant = false, guiActiveEditor = true, guiName = "Thermocouple")]
-        public string CurrentThermocouple = String.Empty;
-
-        [KSPField(isPersistant = false, guiActiveEditor = true, guiName = "Efficiency")]
-        public double ThermocoupleEfficiency = 0;
-
-
-    // flight scene outputs
-        [KSPField(isPersistant = false, guiActiveEditor = false, guiActive = true, guiName = "Fuel Remaining", guiUnits = "%")]
-        public double FuelRemaining = 100;
-
-
-    // input options
-        // value measured in kilograms, max is 50 kilograms
-        [KSPField(isPersistant = true, guiActiveEditor = true, guiName = "Fuel Mass", guiUnits = "kg"), UI_FloatRange(minValue = 1f, maxValue = 50f, stepIncrement = 1f)]
+        [KSPField]
         public float FuelMass = 5;
 
-        // select the next fuel type
-        [KSPEvent(guiName = "Next Fuel", guiActiveEditor = true, guiActive = false)]
-        public void nextFuel()
-        {
-            selectedFuel++;
-            if (selectedFuel >= FuelTypes.Length)
-                selectedFuel = 0;
-            updateValues();
-        }
+		[KSPField]
+		public float EnergyDensity = 0.46F;
+		private bool toggle = true;
 
-        // select the next thermocouple type
-        [KSPEvent(guiName = "Next Thermocouple", guiActiveEditor = true, guiActive = false)]
-        public void nextThermocouple()
-        {
-            selectedThermocouple++;
-            if (selectedThermocouple >= ThermocoupleTypes.Length)
-                selectedThermocouple = 0;
-            updateValues();
-        }
+    // information output
+        [KSPField(guiName = "Power Output", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " Ec/s")]
+		private float PowerOutput = 0;
 
+        [KSPField(guiName = "Half-life", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
+		public float Halflife = 8.352F;
 
-        // Game predetermined updates
-        // updates part values once every Unity FixedUpdate cycle (once per physics frame) once the part has been activated. 
-        public override void OnFixedUpdate()
-        {
-            updateValues();
-        }
+        [KSPField(guiName = "Efficiency", isPersistant = false, guiActiveEditor = true)]
+		public float ThermocoupleEfficiency = 0.05F;
 
-        public override void OnUpdate()
-        {
-            part.RequestResource( "ElectricCharge", -getPowerOutput() );
-        }
+		//[KSPField(guiName = "Output at 3/4", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
+		public double LifePoint1 = 0;
 
-        public override void OnAwake()
-        {
-            updateValues();
-            if (calculateDecay() < 6.25)
-                ScreenMessages.PostScreenMessage("The RTG is operating below one sixteenth efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
-            else if (calculateDecay() < 12.5)
-                ScreenMessages.PostScreenMessage("The RTG is operating at one eighth efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
-            else if (calculateDecay() < 25)
-                ScreenMessages.PostScreenMessage("The RTG is operating at one quarter efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
-            else if (calculateDecay() < 50)
-                ScreenMessages.PostScreenMessage("The RTG is operating at one half efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
-        }
+		//[KSPField(guiName = "Time > 1/2 Power", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
+		public double LifePoint2 = 0;
+
+    // flight scene outputs
+        [KSPField(guiName = "Fuel Remaining", isPersistant = false, guiActiveEditor = false, guiActive = true, guiUnits = "%")]
+		public float FuelRemaining = 100;
+
+    /* ==================================
+     *  UNITY FUNCTIONS
+     * ================================== */
+
+		public override void OnFixedUpdate()
+		{
+			UpdateModule();
+		}
+
+		public override void OnUpdate()
+		{
+			part.RequestResource("ElectricCharge", -GetPowerOutput() * TimeWarp.deltaTime);
+		}
+
+		public override void OnAwake()
+		{
+			UpdateModule();
+			if (CalculateDecay() < 6.25)
+				ScreenMessages.PostScreenMessage("The RTG is operating below one sixteenth efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
+			else if (CalculateDecay() < 12.5)
+				ScreenMessages.PostScreenMessage("The RTG is operating at one eighth efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
+			else if (CalculateDecay() < 25)
+				ScreenMessages.PostScreenMessage("The RTG is operating at one quarter efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
+			else if (CalculateDecay() < 50)
+				ScreenMessages.PostScreenMessage("The RTG is operating at one half efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
+		}
 
         // module info as displayed in VAB/SPH
-        public override string GetInfo()
-        {
-            string s = "Nuclear based RTG with various fuel and thermocouple types and selectable fuel mass.\nFine tune your energy production needs on a per-mission basis.";
-            return s;
-        }
+		public override string GetInfo()
+		{
+			string s = "Output power decays over time.\n\n";
+			s += "Half-life: " + Halflife + " years\n";
+			return s;
+		}
+
+    /* ==================================
+     *  MODUEL FUNCTIONS
+     * ================================== */
 
         // recalculate the display values based on selected fuel type
-        private void updateValues()
-        {
-            CurrentFuel = FuelTypes[selectedFuel];
-            Halflife = Halflives[selectedFuel];
-            PowerOutput = getPowerOutput();
+		private void UpdateModule()
+		{
+			PowerOutput = (float)GetPowerOutput();
+			FuelRemaining = (float)Math.Round(CalculateDecay(), 0);
+			LifePoint1 = CalculateLifepoint(0.75);
+			LifePoint2 = CalculateLifepoint(0.5);
+		}
 
-            CurrentThermocouple = ThermocoupleTypes[selectedThermocouple];
-            ThermocoupleEfficiency = ThermocoupleEfficiencies[selectedThermocouple];
+		private double CalculateDecay()
+		{
+			double missionTime;
+			if (HighLogic.LoadedSceneIsEditor)
+				missionTime = 0;
+			else
+				missionTime = vessel.missionTime;
 
-            //ScreenMessages.PostScreenMessage("Current Thermocouple: " + CurrentThermocouple + ".\nEfficiency " + ThermocoupleEfficiency + ".", 5f, ScreenMessageStyle.UPPER_CENTER);
+			// mission time / seconds in minute / minutes in hour / hours in day / days in year
+			double elapsedYears = missionTime / 60 / 60 / 6 / 426.08;
 
-            FuelRemaining = Math.Round(calculateDecay(), 0);
-        }
+			return 100 * Math.Pow(2, (-elapsedYears / Halflife));
+		}
 
-        private double calculateDecay()
-        {
-            double missionTime;
-            if (HighLogic.LoadedSceneIsEditor)
-                missionTime = 0;
-            else
-                missionTime = vessel.missionTime;
+		private double GetPowerOutput()
+		{
+			// energy output = Decay Rate * power from fuel * efficiency of thermocouple / some value that makes it right?
+			return Math.Round( ( ( CalculateDecay() * ( EnergyDensity * FuelMass ) * ThermocoupleEfficiency ) / 15 ), 2 );
+		}
 
-            // mission time / seconds in minute / minutes in hour / hours in day / days in year
-            double elapsedYears = missionTime / 60 / 60 / 6 / 426.08;
-
-            double result = 100 * Math.Pow(2, (-elapsedYears / Halflife));
-
-            //ScreenMessages.PostScreenMessage("RTG Decay calculated for mission time " + missionTime + ".\nAmount Remaining is " + result + ".", 5f, ScreenMessageStyle.UPPER_CENTER);
-
-            return result;
-        }
-
-        private double getPowerOutput()
-        {
-            double reactionPower = (double)Decimal.Multiply(EnergyDensity[selectedFuel], (decimal)FuelMass);
-
-            //ScreenMessages.PostScreenMessage("Reaction power is " + reactionPower + ".", 5f, ScreenMessageStyle.UPPER_CENTER);
-
-            // energy output = Decay Rate * power from fuel * efficiency of thermocouple / some value that makes it right?
-            double energyFromGenerator = (calculateDecay() * reactionPower * ThermocoupleEfficiency) / 15;
-
-            //ScreenMessages.PostScreenMessage("Output power is " + energyFromGenerator + ".", 5f, ScreenMessageStyle.UPPER_CENTER);
-
-            return Math.Round(energyFromGenerator, 2);
-        }
-
+		private double CalculateLifepoint(double lifePoint)
+		{
+			return Math.Round(Halflife - (Halflife * lifePoint), 3);
+		}
     }
 }

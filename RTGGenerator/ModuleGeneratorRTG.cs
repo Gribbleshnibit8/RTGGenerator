4c44073a84a518ad74f251715ddfb0b3fd47b634
Gribbleshnibit8 <gribbleshnibit8@gmail.com>
2015-01-15 11:21:45 -0600

    Created a resource loader to load custom fuel types for use in RTGs

--- RTGGenerator/ModuleGeneratorRTG.cs
+++ RTGGenerator/ModuleGeneratorRTG.cs
@@ -5,73 +5,245 @@
  * GNU GENERAL PUBLIC LICENSE Version 3
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using UnityEngine;
-using KSP;
 
-// fuel types are:   Strontium-90      Plutonium-238   Americium-241
-//      Half-life:   28.8 years        87.7 years      432 years
-// Energy Density:   0.46kw/kg         0.54kw/kg       0.135kw/kg
-//       Watts/kg:   460W/kg           500W/kg         135W/kg
-//    Cost/Weight:   very cheap        expensive       less expensive
-
-// half-lives measured in Kerbin years, where a year converts as follows:
-//          Hours       Days        Months      Years
-// Kerbin   2556.50	    426.08	    66.23	    1.00
-// Earth    2,556.48    106.52      3.50        0.29
-
+// ReSharper disable InconsistentNaming
 namespace RTGGenerator
 {
     [KSPModule("RTG Power Supply")]
     public class ModuleGeneratorRTG : PartModule
     {
-        [KSPField]
-        public float FuelMass = 5;
+	    readonly ResourceLoader rL = new ResourceLoader();
 
-		[KSPField]
-		public float EnergyDensity = 0.46F;
-		private bool toggle = true;
+		#region Persistent Part Values
 
-    // information output
+		[KSPField(isPersistant = true)]
+		public RtgFuelDefinition resource = null;
+
+		#endregion
+
+		#region Module Input Values
+
+		[KSPField(isPersistant = true, guiActiveEditor = true, guiActive = true)]
+		public string fuel = "Blutonium-238";
+
+		[KSPField(isPersistant = true)]
+		public float fuelMass = 7.8F;
+
+		[KSPField(guiName = "Efficiency", isPersistant = true, guiActiveEditor = true)]
+		public float efficiency = 0.05F;
+
+		#endregion
+
+		#region Editor Info Output
+
         [KSPField(guiName = "Power Output", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " Ec/s")]
-		private float PowerOutput = 0;
+		private double PowerOutput;
 
-        [KSPField(guiName = "Half-life", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
-		public float Halflife = 8.352F;
+		[KSPField(guiName = "Half-life", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
+		public double Halflife = 25.43;
 
-        [KSPField(guiName = "Efficiency", isPersistant = false, guiActiveEditor = true)]
-		public float ThermocoupleEfficiency = 0.05F;
-
-		//[KSPField(guiName = "Output at 3/4", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
+		[KSPField(guiName = "Time to 3/4 output", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
 		public double LifePoint1 = 0;
 
-		//[KSPField(guiName = "Time > 1/2 Power", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
+		[KSPField(guiName = "Time to 1/4 output", isPersistant = false, guiActiveEditor = true, guiActive = true, guiUnits = " years")]
 		public double LifePoint2 = 0;
 
-    // flight scene outputs
-        [KSPField(guiName = "Fuel Remaining", isPersistant = false, guiActiveEditor = false, guiActive = true, guiUnits = "%")]
+		#endregion
+
+		#region Editor Events
+
+		[KSPEvent(guiName = "Next Fuel", guiActiveEditor = true)]
+		public void SelectNextFuel()
+		{
+			Debug.LogWarning("[RRTG] Changing resource: " + resource);
+
+			rL.RtgFuelDefinitions.Dump();
+
+			//resource = rL.RtgFuelDefinitions.GetNextDefinition(resource);
+			//UpdateResource();
+			//UpdateModule();
+		}
+
+		#endregion
+
+		#region Flight Scene Outputs
+
+		[KSPField(guiName = "Fuel Remaining", isPersistant = false, guiActiveEditor = false, guiActive = true, guiUnits = "%")]
 		public float FuelRemaining = 100;
 
-    /* ==================================
-     *  UNITY FUNCTIONS
-     * ================================== */
+	#endregion
+
+		#region Unity Functions
 
 		public override void OnFixedUpdate()
 		{
+			base.OnFixedUpdate();
+			Debug.LogWarning("[RRTG] Fixed Update");
 			UpdateModule();
 		}
 
 		public override void OnUpdate()
 		{
+			base.OnUpdate();
 			part.RequestResource("ElectricCharge", -GetPowerOutput() * TimeWarp.deltaTime);
 		}
 
-		public override void OnAwake()
+		public override string GetInfo()
 		{
 			UpdateModule();
+			base.GetInfo();
+			var sb = new StringBuilder();
+			sb.Append("Output power decays over time.");
+			sb.Append("\n\n");
+
+			// Output fuel info
+			sb.Append("Fuel: ");
+			sb.Append(fuel);
+			sb.Append("\n");
+			sb.Append("Half-life: ");
+			sb.Append(GetDayOrYears(Halflife));
+
+			// Output life-point info
+			sb.Append("Time to 3/4 output: ");
+			sb.Append(GetDayOrYears(LifePoint1));
+			sb.Append("Time to 1/4 output: ");
+			sb.Append(GetDayOrYears(LifePoint2));
+
+			sb.Append("\n\n");
+			sb.Append("Output: ");
+			sb.Append(PowerOutput);
+			sb.Append(" Ec/s");
+
+			return sb.ToString();
+		}
+
+		/// <summary>
+		/// Called when the game is loading the part information. It comes from: the part's cfg file,
+        /// the .craft file, the persistence file, or the quicksave file.
+		/// </summary>
+		/// <param name="node"></param>
+		public override void OnLoad(ConfigNode node)
+		{
+			if (node.HasValue("fuel"))
+			{
+				Debug.LogWarning("[RRTG] : On Load : Loaded the config node");
+				fuel = node.GetValue("fuel");
+				fuelMass = (float)Convert.ToDouble(node.GetValue("fuelMass"));
+				efficiency = (float) Convert.ToDouble(node.GetValue("efficiency"));
+				Debug.LogWarning("[RRTG] : Fuel:" + fuel + " : fuelMass:" + fuelMass + " : efficiency:" + efficiency);
+			}
+
+
+			resource = LoadResource();
+			Debug.LogWarning("[RRTG] : OnLoad : Loaded resource : " + resource);
+			UpdateModule();
+		}
+
+	#endregion
+
+	#region Module Functions
+
+		/// <summary>
+		/// Loads the resource data from the resource list. Returns a default resource if none found.
+		/// </summary>
+		/// <returns></returns>
+		private RtgFuelDefinition LoadResource()
+		{
+			RtgFuelDefinition newResource;
+			Debug.LogWarning("[RRTG] Looking to load : " + fuel);
+			if (rL.RtgFuelDefinitions.Contains(fuel))
+			{
+				newResource = rL.RtgFuelDefinitions.RetreiveByName(fuel);
+				Debug.LogWarning("[RRTG] Found resource " + newResource);
+			}
+			else 
+				newResource = new RtgFuelDefinition();
+
+			return newResource;
+		}
+
+	    /// <summary>
+	    /// Updates the displayed values
+	    /// </summary>
+		private void UpdateModule()
+	    {
+		    Halflife = resource.HalfLife;
+			PowerOutput = GetPowerOutput();
+			FuelRemaining = (float)Math.Round(CalculateDecay(), 0);
+			LifePoint1 = CalculateLifepoint(0.75);
+			LifePoint2 = CalculateLifepoint(0.25);
+		}
+
+	    private void UpdateResource()
+	    {
+		    fuel = resource.Name;
+		    Halflife = (float)resource.HalfLife;
+	    }
+
+		/// <summary>
+		/// Calculates the fuel decay based on the mission time.
+		/// In the editor it will return a decay of 0
+		/// </summary>
+		/// <returns></returns>
+		private double CalculateDecay()
+		{
+			double missionTime = !HighLogic.LoadedSceneIsEditor ? 0 : vessel.missionTime;
+
+			// mission time / seconds in minute / minutes in hour / hours in day / days in year
+			var elapsedYears = missionTime / 60 / 60 / 6 / 426.08;
+
+			return 100 * Math.Pow(2, (-elapsedYears / resource.HalfLife));
+		}
+
+		/// <summary>
+		/// Calculates the output in Ec/s based on the part's settings
+		/// </summary>
+		/// <returns></returns>
+		private double GetPowerOutput()
+		{
+			// energy output = Decay Rate * power from fuel * efficiency of thermocouple / some value that makes it right?
+			return Math.Round(((CalculateDecay() * (resource.EnergyDensity * fuelMass) * efficiency) / 28), 2);
+		}
+
+		/// <summary>
+		/// Calculates the time until the output will be the lifePoint percentage
+		/// </summary>
+		/// <param name="lifePoint">A value from 0.0 to 1.0 as a percentage of power output</param>
+		/// <returns></returns>
+		private double CalculateLifepoint(double lifePoint)
+		{
+			return Math.Round(Halflife - (Halflife * lifePoint), 3);
+		}
+
+		/// <summary>
+		/// Takes a double and returns a string with the suffix in number of days or years in Kerbin time
+		/// </summary>
+		/// <param name="time"></param>
+		/// <returns></returns>
+	    private string GetDayOrYears(double time)
+		{
+			time = Math.Round(time, 3);
+			var sb = new StringBuilder();
+			if (time < 1)
+			{
+				// Get the half-life in terms of days instead
+				var temp = Math.Round(time * 426);
+				sb.Append(temp);
+				sb.Append(" days");
+			}
+			else
+			{
+				sb.Append(time);
+				sb.Append(" years");
+			}
+			sb.Append("\n");
+		    return sb.ToString();
+		}
+
+	    private void EfficiencyStatusMessage()
+	    {
 			if (CalculateDecay() < 6.25)
 				ScreenMessages.PostScreenMessage("The RTG is operating below one sixteenth efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
 			else if (CalculateDecay() < 12.5)
@@ -80,52 +252,17 @@ namespace RTGGenerator
 				ScreenMessages.PostScreenMessage("The RTG is operating at one quarter efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
 			else if (CalculateDecay() < 50)
 				ScreenMessages.PostScreenMessage("The RTG is operating at one half efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
-		}
+			else if (CalculateDecay() < 75)
+				ScreenMessages.PostScreenMessage("The RTG is operating at three quarter efficiency.", 5f, ScreenMessageStyle.UPPER_CENTER);
+	    }
 
-        // module info as displayed in VAB/SPH
-		public override string GetInfo()
-		{
-			string s = "Output power decays over time.\n\n";
-			s += "Half-life: " + Halflife + " years\n";
-			return s;
-		}
+		#endregion
 
-    /* ==================================
-     *  MODUEL FUNCTIONS
-     * ================================== */
+		// NOTES
 
-        // recalculate the display values based on selected fuel type
-		private void UpdateModule()
-		{
-			PowerOutput = (float)GetPowerOutput();
-			FuelRemaining = (float)Math.Round(CalculateDecay(), 0);
-			LifePoint1 = CalculateLifepoint(0.75);
-			LifePoint2 = CalculateLifepoint(0.5);
-		}
+		// To calculate the volume of fuel any of the RTGs can store use
+		// Volume = Density / Mass
 
-		private double CalculateDecay()
-		{
-			double missionTime;
-			if (HighLogic.LoadedSceneIsEditor)
-				missionTime = 0;
-			else
-				missionTime = vessel.missionTime;
 
-			// mission time / seconds in minute / minutes in hour / hours in day / days in year
-			double elapsedYears = missionTime / 60 / 60 / 6 / 426.08;
-
-			return 100 * Math.Pow(2, (-elapsedYears / Halflife));
-		}
-
-		private double GetPowerOutput()
-		{
-			// energy output = Decay Rate * power from fuel * efficiency of thermocouple / some value that makes it right?
-			return Math.Round( ( ( CalculateDecay() * ( EnergyDensity * FuelMass ) * ThermocoupleEfficiency ) / 15 ), 2 );
-		}
-
-		private double CalculateLifepoint(double lifePoint)
-		{
-			return Math.Round(Halflife - (Halflife * lifePoint), 3);
-		}
     }
 }
---
+++ RTGGenerator/ResourceLoader.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using UnityEngine;
+
+// ReSharper disable LoopCanBeConvertedToQuery
+
+namespace RTGGenerator
+{
+	[KSPAddon(KSPAddon.Startup.MainMenu, false)]
+	public class ResourceLoader : MonoBehaviour
+	{
+
+		public RtgFuelList RtgFuelDefinitions = new RtgFuelList();
+
+		void Awake()
+		{
+			Debug.Log("RRTG Main Menu [" + this.GetInstanceID().ToString("X") + "][" + Time.time.ToString("0.0000") + "]: Awake: " + this.name);
+
+			foreach (var node in GameDatabase.Instance.GetConfigNodes("RESOURCE_DEFINITION"))
+			{
+				if (node.HasValue("halfLife"))
+				{
+					var newFuel = new RtgFuelDefinition(node);
+					if (RtgFuelDefinitions.Contains(newFuel))
+						Debug.LogWarning("[RRTG] Ignored duplicate fuel type " + node.GetValue("name"));
+					else
+						RtgFuelDefinitions.Add(newFuel);
+				}
+			}
+
+			RtgFuelDefinitions.Dump();
+		}
+	}
+
+
+	public class RtgFuelList : List<RtgFuelDefinition>
+	{
+
+		public bool Contains(string toFind)
+		{
+			foreach (var fuel in this)
+			{
+				Debug.LogWarning("[RRTG] : " + fuel.Name + " compared to " + toFind);
+				if (fuel.Name.Equals(toFind))
+					return true;
+			}
+
+			return false;
+		}
+
+		/// <summary>
+		/// Returns a RtgFuelDefinition from the name of a fuel resource
+		/// </summary>
+		/// <param name="toFind">The name of the fuel resource to find</param>
+		/// <returns></returns>
+		public RtgFuelDefinition RetreiveByName(string toFind)
+		{
+			foreach (RtgFuelDefinition fuel in this)
+			{
+				Debug.LogWarning("[RRTG] Retreive by name " + fuel.Name);
+				if (fuel.Name == toFind) return fuel;
+			}
+				
+			return null;
+		}
+
+		public void Dump()
+		{
+			int counter = 0;
+			foreach (var fuel in this)
+			{
+				counter++;
+				Debug.LogWarning("[RRTG] : Fuel list dump : " + counter + fuel);
+			}
+		}
+
+	}
+
+
+	public class RtgFuelDefinitionList : KeyedCollection<string, RtgFuelDefinition>, IConfigNode
+	{
+		protected override string GetKeyForItem(RtgFuelDefinition item)
+		{
+			return item.Name;
+		}
+
+		public void Load(ConfigNode node)
+		{
+			foreach (var fuelNode in node.GetNodes("RESOURCE_DEFINITION"))
+			{
+				Add(new RtgFuelDefinition(fuelNode));
+			}
+		}
+
+		public void Save(ConfigNode node)
+		{
+			foreach (var fuel in this)
+			{
+				ConfigNode tankNode = new ConfigNode("TANK");
+				fuel.Save(tankNode);
+				node.AddNode(tankNode);
+			}
+		}
+
+		/// <summary>
+		/// Returns a RtgFuelDefinition from the name of a fuel resource
+		/// </summary>
+		/// <param name="toFind">The name of the fuel resource to find</param>
+		/// <returns></returns>
+		public RtgFuelDefinition FindByName(string toFind)
+		{
+			foreach (RtgFuelDefinition fuel in this)
+				if (fuel.Name == toFind) return fuel;
+			return null;
+		}
+
+		public RtgFuelDefinition GetNextDefinition(RtgFuelDefinition node)
+		{
+			Debug.LogWarning("[RRTG] Finding next resource definition");
+			for (int index = 0; index < Count; index++)
+			{
+				var fuel = this[index];
+				if (fuel == node && this[Count-1] != node)
+				{
+					Debug.LogWarning("[RRTG] Found next resource: " + this[index+1]);
+					return this[index + 1];
+				}
+			}
+			return this[0];
+		}
+
+		public RtgFuelDefinition GetPreviousDefinition(RtgFuelDefinition node)
+		{
+			for (int index = Count; index > 0; index--)
+			{
+				var fuel = this[index];
+				if (fuel == node && this[0] != node)
+					return this[index - 1];
+			}
+			return this[Count-1];
+		}
+
+		public void Dump()
+		{
+			int counter = 0;
+			foreach (var fuel in this)
+			{
+				counter++;
+				Debug.LogWarning("[RRTG] : Fuel list dump : " + counter + fuel);
+			}
+		}
+
+
+	}
+
+
+	// ReSharper disable InconsistentNaming
+	public class RtgFuelDefinition : IConfigNode
+	{
+
+		#region Fields
+
+		[Persistent]
+		private string name;
+
+		[Persistent]
+		private string halfLife;
+
+		[Persistent]
+		private string energyDensity;
+
+		[Persistent]
+		private string density;
+
+		[Persistent]
+		private string cost;
+
+		#endregion
+
+		#region Constructors
+
+		public RtgFuelDefinition()
+		{
+			name = "Blutonium-238";
+			halfLife = 25.43.ToString();
+			energyDensity = 0.54.ToString();
+			density = 0.0.ToString();
+			cost = 0.0.ToString();
+		}
+
+		public RtgFuelDefinition(ConfigNode node)
+		{
+			Load(node);
+		}
+
+		#endregion
+
+		#region Accessors
+
+		public string Name { get { return name; } }
+
+		public double HalfLife { get { return Convert.ToDouble(halfLife); } }
+
+		public double EnergyDensity { get { return Convert.ToDouble(energyDensity); } }
+
+		public double Density { get { return Convert.ToDouble(density); } }
+
+		public double Cost { get { return Convert.ToDouble(cost); } }
+
+		#endregion
+
+		public void Load(ConfigNode node)
+		{
+			ConfigNode.LoadObjectFromConfig(this, node);
+		}
+
+		public void Save(ConfigNode node)
+		{
+			ConfigNode.CreateConfigFromObject(this, node);
+		}
+
+		public override string ToString()
+		{
+			return "Name: " + name + Environment.NewLine +
+				   "Half-life: " + halfLife + Environment.NewLine +
+				   "Energy Density: " + energyDensity;
+		}
+	}
+
+}
